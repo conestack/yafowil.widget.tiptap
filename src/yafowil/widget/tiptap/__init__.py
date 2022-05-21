e20c510a71f14986395318cbca86055ce546e53c
Robert Niederreiter <rnix@squarewave.at>
2022-05-21 06:52:37 +0200

    "Register resources with webresource"

--- src/yafowil/widget/tiptap/__init__.py
+++ src/yafowil/widget/tiptap/__init__.py
@@ -1,9 +1,49 @@
 from yafowil.base import factory
 from yafowil.utils import entry_point
 import os
+import webresource as wr
 
 
-resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
+
+
+##############################################################################
+# Default
+##############################################################################
+
+# webresource ################################################################
+
+scripts = wr.ResourceGroup(name='scripts')
+scripts.add(wr.ScriptResource(
+    name='tiptap-js',
+    # actually it not depends on jquery, but yafowil-tiptap-js does
+    # think about multiple depends values in webresource
+    depends='jquery-js',
+    directory=resources_dir,
+    resource='tiptap.js',
+    compressed='tiptap.min.js'
+))
+scripts.add(wr.ScriptResource(
+    name='yafowil-tiptap-js',
+    depends='tiptap-js',
+    directory=resources_dir,
+    resource='widget.js',
+    compressed='widget.min.js'
+))
+
+styles = wr.ResourceGroup(name='styles')
+styles.add(wr.StyleResource(
+    name='yafowil-tiptap-css',
+    directory=resources_dir,
+    resource='widget.css'
+))
+
+resources = wr.ResourceGroup(name='tiptap-resources')
+resources.add(scripts)
+resources.add(styles)
+
+# B/C resources ##############################################################
+
 js = [{
     'group': 'yafowil.widget.tiptap.dependencies',
     'resource': 'tiptap.js',
@@ -13,21 +53,23 @@ js = [{
     'resource': 'widget.js',
     'order': 21,
 }]
-default_css = [{
+css = [{
     'group': 'yafowil.widget.tiptap.common',
     'resource': 'widget.css',
     'order': 21,
 }]
 
 
+##############################################################################
+# Registration
+##############################################################################
+
 @entry_point(order=10)
 def register():
     from yafowil.widget.tiptap import widget  # noqa
 
+    # Default
     factory.register_theme(
-        'default',
-        'yafowil.widget.tiptap',
-        resourcedir,
-        js=js,
-        css=default_css
+        'default', 'yafowil.widget.tiptap', resources_dir,
+        js=js, css=css, resources=resources
     )
